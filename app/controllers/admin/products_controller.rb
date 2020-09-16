bdf04821bd8ef62df798540f49b86249972eccf8
Camilo Gomez <camilogomezmusic@gmail.com>
2020-09-16 13:16:47 +0000

    Add basic http authentication for admin routes

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,7 +1,11 @@
 class Admin::ProductsController < ApplicationController
 
+
+  http_basic_authenticate_with name: ENV['HTTP_BASIC_AUTH_USER'], password: ENV['HTTP_BASIC_AUTH_PASSWORD']
+  
   def index
     @products = Product.order(id: :desc).all
+    puts ENV['HTTP_BASIC_AUTH_PASSWORD']
   end
 
   def new
