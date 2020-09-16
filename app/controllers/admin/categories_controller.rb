33bc9bbb676d35a479833dc4909104fe456af9e5
Camilo Gomez <camilogomezmusic@gmail.com>
2020-09-16 23:06:09 +0000

    Add admin-categories route, create admin_categories views

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,8 @@
+class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV['HTTP_BASIC_AUTH_USER'], password: ENV['HTTP_BASIC_AUTH_PASSWORD']
+
+
+  def index
+  end
+  
+end
